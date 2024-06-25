de858e103edfc56eb7458773e0cdfb73490a4534
ZaidSadAlDeen <adens7d7tlah@gmail.com>
2024-06-25 18:23:27 +0300

    task5

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-var language =['c++','java','c'];
-var data="";
-for(var i=0;i<3;i++){
-data+=`<li>`+language[i]+`</li>`;
+var convert = document.querySelector(".exchangeForm");
+var result;
+convert.onsubmit =function(e){
+    var type = document.querySelector(".exchange").value;
+    e.preventDefault();
+    var amount = e.target.elements;
+    var type = amount[1].value;
+    result =amount[0].value;
+    if(type=="dollar"){
+        result=result*3.5;
+    }else if(type=="dinar"){
+        result=result*5;
+    }
+   document.querySelector(".result").textContent=result;
 }
-document.querySelector("ul").innerHTML =data;
+
+
