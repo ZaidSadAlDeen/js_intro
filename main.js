2b30ea41e8360d7ef551e1c189624d788ac4abd7
ZaidSadAlDeen <adens7d7tlah@gmail.com>
2024-07-02 06:56:52 +0300

    task6

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-var convert = document.querySelector(".exchangeForm");
-var result;
-convert.onsubmit =function(e){
-    var type = document.querySelector(".exchange").value;
-    e.preventDefault();
-    var amount = e.target.elements;
-    var type = amount[1].value;
-    result =amount[0].value;
-    if(type=="dollar"){
-        result=result*3.5;
-    }else if(type=="dinar"){
-        result=result*5;
-    }
-   document.querySelector(".result").textContent=result;
+async function getPizza() {
+    const response = await fetch(`https://forkify-api.herokuapp.com/api/search?q=pizza`);
+    const data = await response.json();
+    const recipes = data.recipes;
+    const result = recipes.map(function(ele) {
+        return `
+        <div class="recipes">
+        <h2>${ele.title}</h2>
+        <img src="${ele.image_url}" width=600px>
+        </div>
+        `
+    }).join('');
+    document.querySelector("body").innerHTML = result;
 }
-
-
+getPizza();
